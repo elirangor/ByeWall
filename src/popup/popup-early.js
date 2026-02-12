39ebe6eed4fc0d130c805535bcb1518b8dbc8bf6
elirangor <eliran.gorgov@gmail.com>
2026-02-12 15:10:47 +0200

    Fix dark/light mode

--- src/popup/popup-early.js
+++ src/popup/popup-early.js
@@ -3,10 +3,6 @@
   try {
     if (localStorage.getItem("darkModeEnabled") === "true") {
       document.documentElement.classList.add("dark-mode");
-      var s = document.createElement("style");
-      s.textContent =
-        "html.dark-mode, html.dark-mode body{background:#121212;color:#eaeaea;color-scheme:dark;}";
-      document.head.appendChild(s);
     }
   } catch {}
 })();
